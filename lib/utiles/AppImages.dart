3b568302c388f8317ab7ac9a1ae9f1fd3b8d9b9b
mohamedzakaria9 <mohamed.zakaria.3@hotmail.com>
2025-11-16 15:47:36 +0200

    adding images that will be used in the application and create class to to access them throw static variables

--- lib/utiles/AppImages.dart
+++ lib/utiles/AppImages.dart
@@ -1,3 +1,21 @@
 class AppImages{
   static const String appLogo = "assets/images/AppLogo.png";
+  static const String businessImageDark = "assets/images/BusinessImageDark.png";
+  static const String businessImageLight = "assets/images/BusinessImageLight.png";
+  static const String closeSearchIcon = "assets/images/CloseSearchIcon.png";
+  static const String dropDownIcon = "assets/images/DropDownIcon.png";
+  static const String entertainmentImageDark = "assets/images/EntertainmentImageDark.png";
+  static const String entertainmentImageLight = "assets/images/EntertainmentImageLight.png";
+  static const String generalImageDark = "assets/images/GeneralImageDark.png";
+  static const String generalImageLight = "assets/images/GeneralImageLight.png";
+  static const String healthImageDark = "assets/images/HealthImageDark.png";
+  static const String healthImageLight = "assets/images/HealthImageLight.png";
+  static const String scienceImageDark = "assets/images/ScienceImageDark.png";
+  static const String scienceImageLight = "assets/images/ScienceImageLight.png";
+  static const String searchIcon = "assets/images/SearchIcon.png";
+  static const String sportImageDark = "assets/images/SportImageDark.png";
+  static const String sportImageLight = "assets/images/SportsImageLight.png";
+  static const String technologyImageDark = "assets/images/TechnologyImageDark.png";
+  static const String technologyImageLight = "assets/images/TechnologyImageLight.png";
+  static const String viewAllIcon = "assets/images/ViewAllIcon.png";
 }
